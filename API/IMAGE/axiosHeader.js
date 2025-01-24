81985a9d52eb843b5ba47be70f3ef336962b5a1b
abhishekkumar848 <153023426+abhishekkumar848@users.noreply.github.com>
2025-01-24 06:55:24 +0530

    24 jan 2025

--- API/IMAGE/axiosHeader.js
+++ API/IMAGE/axiosHeader.js
@@ -1,4 +1,11 @@
-let url = 'https://icanhazdadjoke.com/';
+let urldata = 'https://icanhazdadjoke.com/';
 async function getdata() {
-    
+    try {
+        const head = {headers : {Accept : "application/json"}}
+        let res = await axios.get(urldata,head);
+        console.log(res.data);
+    } catch (error) {
+        console.log(error)
+    }
 }
+getdata();
